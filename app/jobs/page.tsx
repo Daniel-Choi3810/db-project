130c65e0034da30b322056f9e20a96dc3b4fa688
Daniel Choi <63138113+Daniel-Choi3810@users.noreply.github.com>
2023-11-20 17:08:39 -0500

    Implements basic search

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,30 +1,40 @@
 import PaginationControls from "@/components/common/PaginationControls";
 import JobCard from "@/components/jobs/JobCard";
+import SearchBar from "@/components/jobs/SearchBar";
 import prisma from "@/lib/prisma";
 import React from "react";
 
 export default async function Jobs({
   searchParams,
 }: {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: { query: string; [key: string]: string | string[] | undefined };
 }) {
+  const query = searchParams?.query || "";
   const page = parseInt(searchParams?.page as string) || 1;
   const perPage = parseInt(searchParams?.per_page as string) || 50;
 
-  // Calculate the number of job postings to skip (offset)
   const offset = (page - 1) * perPage;
 
-  // Fetch the paginated job postings directly from the database
-  const jobPostings: any[] = await prisma.$queryRaw`
-    SELECT 
+  const whereClause = query
+    ? `WHERE JobPostings.title LIKE '%${query}%' OR Company.name LIKE '%${query}%' 
+    OR JobPostings.description LIKE '%${query}%' 
+    OR JobPostings.salary LIKE '%${query}%'
+    OR JobPostings.workType LIKE '%${query}%'
+    OR JobPostings.jobPostURL LIKE '%${query}%'`
+    : "";
+
+  // Use the whereClause in the SQL query
+  const jobPostings: any[] = await prisma.$queryRawUnsafe(
+    `SELECT 
       JobPostings.*, 
       Company.name AS companyName 
     FROM 
       JobPostings 
     JOIN 
       Company ON JobPostings.companyID = Company.companyID
-    LIMIT ${perPage} OFFSET ${offset};
-  `;
+    ${whereClause}
+    LIMIT ${perPage} OFFSET ${offset};`
+  );
 
   // Fetch the total count of job postings for pagination controls
   const totalPostings: any[] =
@@ -33,6 +43,7 @@ export default async function Jobs({
 
   return (
     <div className="relative flex flex-col justify-center items-center mt-20">
+      <SearchBar />
       <div className="grid grid-cols-3 gap-4">
         {jobPostings.map((jobPosting: any) => (
           <JobCard
